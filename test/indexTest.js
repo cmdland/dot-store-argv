18db63fa9aa1d8dc7493344ef045669915b4625a
winton <mail@wintoni.us>
2018-09-24 02:11:20 -0400

    Upgrade packages

--- test/indexTest.js
+++ test/indexTest.js
@@ -3,9 +3,10 @@ import argv from "../dist/argv"
 
 test("parse argv", async () => {
   const store = argv(new Store())
-  await store.set("argv.test.alias", { world: ["w"] })
-  await store.set("argv.test.raw", ["hello", "-w"])
-  expect(store.get("argv.test.parsed")).toEqual({
+  await store.argv("test", ["hello", "-w"], {
+    alias: { world: ["w"] },
+  })
+  expect(store.get("test")).toEqual({
     _: ["hello"],
     w: true,
     world: true,
