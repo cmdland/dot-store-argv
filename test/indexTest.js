6d532452c4438eb0b2b4a843a2ffef2b4fb14ee6
winton <mail@wintoni.us>
2018-10-10 15:12:39 -0400

    Upgrade dot-event and dot-store

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,12 +1,21 @@
-import Store from "dot-store"
+import dotEvent from "dot-event"
+import dotStore from "dot-store"
 import argv from "../dist/argv"
 
 test("parse argv", async () => {
-  const store = argv(new Store())
-  await store.argv("test", ["hello", "-w"], {
-    alias: { world: ["w"] },
+  const events = dotEvent()
+  const store = dotStore(events)
+
+  argv({ events, store })
+
+  await events.argv("test", {
+    argv: ["hello", "-w"],
+    options: {
+      alias: { world: ["w"] },
+    },
   })
-  expect(store.get("test")).toEqual({
+
+  expect(store.get("argv.test")).toEqual({
     _: ["hello"],
     w: true,
     world: true,
