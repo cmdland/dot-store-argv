dfbb86be563be59d471fb1a63eb695e5abc56316
winton <mail@wintoni.us>
2018-11-16 00:50:30 -0500

    Cache raw argv value, simplify props

--- test/indexTest.js
+++ test/indexTest.js
@@ -2,7 +2,7 @@ import dotEvent from "dot-event"
 import dotStore from "dot-store"
 import argv from "../dist/argv"
 
-test("parse argv", async () => {
+test("parse argv (and reload)", async () => {
   const events = dotEvent()
   const store = dotStore(events)
 
@@ -10,14 +10,21 @@ test("parse argv", async () => {
 
   await events.argv("test", {
     argv: ["hello", "-w"],
+  })
+
+  expect(store.get("test")).toEqual({
+    opts: { _: ["hello"], w: true },
+    raw: ["hello", "-w"],
+  })
+
+  await events.argv("test", {
     options: {
       alias: { world: ["w"] },
     },
   })
 
-  expect(store.get("argv.test")).toEqual({
-    _: ["hello"],
-    w: true,
-    world: true,
+  expect(store.get("test")).toEqual({
+    opts: { _: ["hello"], w: true, world: true },
+    raw: ["hello", "-w"],
   })
 })
